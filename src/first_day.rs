d50326534b2758a72571c71a68f40cd5eb650242
Eric Puentes <eric.david2333@gmail.com>
2021-12-01 14:28:12 -0500

    day 1

---
+++ src/first_day.rs
@@ -0,0 +1,50 @@
+pub fn run_first_day() {
+    let list = crate::lines_from_file("inputs/day1.txt").expect("Could not load lines");
+    let measures = list
+        .into_iter()
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+    let count = count_increased(measures.clone());
+    println!("The measures increased: {} times", count);
+    let count = count_increased(clean_measures(measures));
+    println!("The measures increased: {} times", count);
+}
+
+fn count_increased(measures: Vec<i32>) -> i32 {
+    let mut count = -1;
+    measures.into_iter().fold(0, |acc, m| {
+        if m > acc {
+            count += 1;
+        }
+        m
+    });
+    count
+}
+
+fn clean_measures(measures: Vec<i32>) -> Vec<i32> {
+    let mut new_measurents = Vec::new();
+    for (i, m) in measures.iter().enumerate() {
+        if i + 2 < measures.len() {
+            new_measurents.push(m + measures[i + 1] + measures[i + 2])
+        }
+    }
+    new_measurents
+}
+
+#[test]
+fn count_increased_test() {
+    let measures = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+    let count = count_increased(measures);
+    assert_eq!(7, count);
+}
+
+#[test]
+fn clean_measures_test() {
+    let measures = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+    let count = clean_measures(measures);
+    assert_eq!(8, count.len());
+    let mut it = count.into_iter();
+    assert_eq!(607, it.next().unwrap());
+    assert_eq!(618, it.next().unwrap());
+    assert_eq!(792, it.last().unwrap());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod first_day;
+
 use std::{
     fs::File,
     io::{BufRead, BufReader, Result},
@@ -6,6 +8,7 @@ use std::{
 
 fn main() {
     println!("Hello, world!");
+    first_day::run_first_day();
 }
 
 pub fn lines_from_file(filename: impl AsRef<Path>) -> Result<Vec<String>> {
