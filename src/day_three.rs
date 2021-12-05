92422aaa95a7579b5e91251014822ec30cc167b6
Eric Puentes <eric.david2333@gmail.com>
2021-12-04 21:30:50 -0500

    refactor day3

--- src/day_three.rs
+++ src/day_three.rs
@@ -1,32 +1,68 @@
 use anyhow::Result;
 
-pub fn run_day_three() -> Result<()> {
-    let input: Vec<Vec<char>> = std::fs::read_to_string("inputs/day3.txt")?
-        .lines()
-        .map(|s| s.chars().collect())
-        .collect();
+use crate::runner::Day;
 
-    let power = power_consumption(input.clone())?;
-    println!("The power consumption is: {}", power);
-    let ls = life_supply(input)?;
-    println!("The life supply is: {}", ls);
-    Ok(())
+pub struct Day3 {}
+
+impl Day<Vec<Vec<char>>, isize> for Day3 {
+    fn parse_input(input: &str) -> Result<Vec<Vec<char>>> {
+        let r = input.lines().map(|s| s.chars().collect()).collect();
+        Ok(r)
+    }
+
+    fn part_one(input: &Vec<Vec<char>>) -> Result<isize> {
+        let (gama, epsilon) = binaries_for_power(input)?;
+        let gama = isize::from_str_radix(gama.as_str(), 2)?;
+        let epsilon = isize::from_str_radix(epsilon.as_str(), 2)?;
+        Ok(gama * epsilon)
+    }
+
+    fn part_two(input: &Vec<Vec<char>>) -> Result<isize> {
+        let des = |ones: u32, zeros: u32| -> char {
+            let ch: char;
+            if ones >= zeros {
+                ch = '0';
+            } else {
+                ch = '1';
+            }
+            ch
+        };
+        let oxigen = binaries_for_life(input.clone(), des)?;
+        let des = |ones: u32, zeros: u32| -> char {
+            let ch: char;
+            if ones >= zeros {
+                ch = '1';
+            } else {
+                ch = '0';
+            }
+            ch
+        };
+        let co_scrubber = binaries_for_life(input.to_vec(), des)?;
+        let oxigen = isize::from_str_radix(oxigen.as_str(), 2)?;
+        let co_scrubber = isize::from_str_radix(co_scrubber.as_str(), 2)?;
+        Ok(oxigen * co_scrubber)
+    }
 }
 
-fn binaries_for_power(input: Vec<Vec<char>>) -> Result<(String, String)> {
+fn count(input: &[Vec<char>], i: usize) -> (u32, u32) {
+    let mut ones: u32 = 0;
+    let mut zeros: u32 = 0;
+    for line in input {
+        match line[i] {
+            '0' => zeros += 1,
+            '1' => ones += 1,
+            _ => panic!("invalid input"),
+        }
+    }
+    (ones, zeros)
+}
+
+fn binaries_for_power(input: &[Vec<char>]) -> Result<(String, String)> {
     let mut gama = String::new();
     let mut epsilon = String::new();
     let len = input[0].len();
     for i in 0..len {
-        let mut ones: u32 = 0;
-        let mut zeros: u32 = 0;
-        for line in &input {
-            match line[i] {
-                '0' => zeros += 1,
-                '1' => ones += 1,
-                _ => panic!("invalid input"),
-            }
-        }
+        let (ones, zeros) = count(input, i);
         if ones > zeros {
             gama.push('1');
             epsilon.push('0');
@@ -38,25 +74,10 @@ fn binaries_for_power(input: Vec<Vec<char>>) -> Result<(String, String)> {
     Ok((gama, epsilon))
 }
 
-fn power_consumption(input: Vec<Vec<char>>) -> Result<isize> {
-    let (gama, epsilon) = binaries_for_power(input)?;
-    let gama = isize::from_str_radix(gama.as_str(), 2)?;
-    let epsilon = isize::from_str_radix(epsilon.as_str(), 2)?;
-    Ok(gama * epsilon)
-}
-
 fn binaries_for_life(mut input: Vec<Vec<char>>, desition: fn(u32, u32) -> char) -> Result<String> {
     let mut i = 0;
     while input.len() > 1 {
-        let mut ones: u32 = 0;
-        let mut zeros: u32 = 0;
-        for line in &input {
-            match line[i] {
-                '0' => zeros += 1,
-                '1' => ones += 1,
-                _ => panic!("invalid input"),
-            }
-        }
+        let (ones, zeros) = count(&input, i);
         let ch = desition(ones, zeros);
         input = input.into_iter().filter(|v| v[i] == ch).collect();
         i += 1;
@@ -65,94 +86,50 @@ fn binaries_for_life(mut input: Vec<Vec<char>>, desition: fn(u32, u32) -> char) 
     Ok(result)
 }
 
-fn life_supply(input: Vec<Vec<char>>) -> Result<isize> {
-    let des = |ones: u32, zeros: u32| -> char {
-        let ch: char;
-        if ones >= zeros {
-            ch = '0';
-        } else {
-            ch = '1';
-        }
-        ch
-    };
-    let oxigen = binaries_for_life(input.clone(), des)?;
-    let des = |ones: u32, zeros: u32| -> char {
-        let ch: char;
-        if ones >= zeros {
-            ch = '1';
-        } else {
-            ch = '0';
-        }
-        ch
-    };
-    let co_scrubber = binaries_for_life(input, des)?;
-    let oxigen = isize::from_str_radix(oxigen.as_str(), 2)?;
-    let co_scrubber = isize::from_str_radix(co_scrubber.as_str(), 2)?;
-    Ok(oxigen * co_scrubber)
-}
 #[cfg(test)]
-mod tests {
+mod tests_day3 {
     use super::*;
+    use crate::runner::Runner;
+    use anyhow::Result;
+
+    #[test]
+    fn run_test() -> Result<()> {
+        let (r1, r2) = Day3::run("inputs/day3.test")?;
+        assert_eq!(r1, 198);
+        assert_eq!(r2, 230);
+        Ok(())
+    }
+
+    const INPUT: &str = include_str!("../inputs/day3.test");
+
+    #[test]
+    fn test_power_part_one() -> Result<()> {
+        let input = Day3::parse_input(INPUT)?;
+        let power = Day3::part_one(&input)?;
+        assert_eq!(power, 198);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part_two() -> Result<()> {
+        let input = Day3::parse_input(INPUT)?;
+        let life_supply = Day3::part_two(&input)?;
+        assert_eq!(life_supply, 230);
+        Ok(())
+    }
 
     #[test]
     fn test_binaries_for_power() -> Result<()> {
-        let input: Vec<Vec<char>> = vec![
-            vec!['0', '0', '1', '0', '0'],
-            vec!['1', '1', '1', '1', '0'],
-            vec!['1', '0', '1', '1', '0'],
-            vec!['1', '0', '1', '1', '1'],
-            vec!['1', '0', '1', '0', '1'],
-            vec!['0', '1', '1', '1', '1'],
-            vec!['0', '0', '1', '1', '1'],
-            vec!['1', '1', '1', '0', '0'],
-            vec!['1', '0', '0', '0', '0'],
-            vec!['1', '1', '0', '0', '1'],
-            vec!['0', '0', '0', '1', '0'],
-            vec!['0', '1', '0', '1', '0'],
-        ];
-        let (gama, epsilon) = binaries_for_power(input)?;
+        let input = Day3::parse_input(INPUT)?;
+        let (gama, epsilon) = binaries_for_power(&input)?;
         assert_eq!(gama, "10110");
         assert_eq!(epsilon, "01001");
         Ok(())
     }
 
     #[test]
-    fn test_power_consumption() -> Result<()> {
-        let input: Vec<Vec<char>> = vec![
-            vec!['0', '0', '1', '0', '0'],
-            vec!['1', '1', '1', '1', '0'],
-            vec!['1', '0', '1', '1', '0'],
-            vec!['1', '0', '1', '1', '1'],
-            vec!['1', '0', '1', '0', '1'],
-            vec!['0', '1', '1', '1', '1'],
-            vec!['0', '0', '1', '1', '1'],
-            vec!['1', '1', '1', '0', '0'],
-            vec!['1', '0', '0', '0', '0'],
-            vec!['1', '1', '0', '0', '1'],
-            vec!['0', '0', '0', '1', '0'],
-            vec!['0', '1', '0', '1', '0'],
-        ];
-        let power = power_consumption(input)?;
-        assert_eq!(power, 198);
-        Ok(())
-    }
-
-    #[test]
     fn test_binaries_for_life_oxygen() -> Result<()> {
-        let input: Vec<Vec<char>> = vec![
-            vec!['0', '0', '1', '0', '0'],
-            vec!['1', '1', '1', '1', '0'],
-            vec!['1', '0', '1', '1', '0'],
-            vec!['1', '0', '1', '1', '1'],
-            vec!['1', '0', '1', '0', '1'],
-            vec!['0', '1', '1', '1', '1'],
-            vec!['0', '0', '1', '1', '1'],
-            vec!['1', '1', '1', '0', '0'],
-            vec!['1', '0', '0', '0', '0'],
-            vec!['1', '1', '0', '0', '1'],
-            vec!['0', '0', '0', '1', '0'],
-            vec!['0', '1', '0', '1', '0'],
-        ];
+        let input = Day3::parse_input(INPUT)?;
         let des = |ones: u32, zeros: u32| -> char {
             let ch: char;
             if ones >= zeros {
@@ -169,20 +146,7 @@ mod tests {
 
     #[test]
     fn test_binaries_for_life_co() -> Result<()> {
-        let input: Vec<Vec<char>> = vec![
-            vec!['0', '0', '1', '0', '0'],
-            vec!['1', '1', '1', '1', '0'],
-            vec!['1', '0', '1', '1', '0'],
-            vec!['1', '0', '1', '1', '1'],
-            vec!['1', '0', '1', '0', '1'],
-            vec!['0', '1', '1', '1', '1'],
-            vec!['0', '0', '1', '1', '1'],
-            vec!['1', '1', '1', '0', '0'],
-            vec!['1', '0', '0', '0', '0'],
-            vec!['1', '1', '0', '0', '1'],
-            vec!['0', '0', '0', '1', '0'],
-            vec!['0', '1', '0', '1', '0'],
-        ];
+        let input = Day3::parse_input(INPUT)?;
         let des = |ones: u32, zeros: u32| -> char {
             let ch: char;
             if ones >= zeros {
@@ -194,27 +158,6 @@ mod tests {
         };
         let co = binaries_for_life(input, des)?;
         assert_eq!(co, "01010");
-        Ok(())
-    }
-
-    #[test]
-    fn test_life_supply() -> Result<()> {
-        let input: Vec<Vec<char>> = vec![
-            vec!['0', '0', '1', '0', '0'],
-            vec!['1', '1', '1', '1', '0'],
-            vec!['1', '0', '1', '1', '0'],
-            vec!['1', '0', '1', '1', '1'],
-            vec!['1', '0', '1', '0', '1'],
-            vec!['0', '1', '1', '1', '1'],
-            vec!['0', '0', '1', '1', '1'],
-            vec!['1', '1', '1', '0', '0'],
-            vec!['1', '0', '0', '0', '0'],
-            vec!['1', '1', '0', '0', '1'],
-            vec!['0', '0', '0', '1', '0'],
-            vec!['0', '1', '0', '1', '0'],
-        ];
-        let life_supply = life_supply(input)?;
-        assert_eq!(life_supply, 230);
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,14 @@ mod runner;
 
 use anyhow::Result;
 use day_one::Day1;
+use day_three::Day3;
 use day_two::Day2;
 use runner::Runner;
 
 fn main() -> Result<()> {
     Day1::run("inputs/day1.input")?;
     Day2::run("inputs/day2.input")?;
-    day_three::run_day_three()?;
+    Day3::run("inputs/day3.input")?;
     day_four::run_day_four("inputs/day4.input")?;
     Ok(())
 }
