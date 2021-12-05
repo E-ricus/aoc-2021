94a6cb6d1be0960f358159a0afeedd9128e51314
Eric Puentes <eric.david2333@gmail.com>
2021-12-04 20:36:12 -0500

    refactor day1

--- src/day_one.rs
+++ src/day_one.rs
@@ -1,17 +1,26 @@
 use anyhow::Result;
 
-pub fn run_day_one() -> Result<()> {
-    let measures: Vec<i32> = std::fs::read_to_string("inputs/day1.txt")?
-        .lines()
-        .into_iter()
-        .filter_map(|s| s.parse().ok())
-        .collect();
+use crate::runner::Day;
 
-    let count = count_increased(measures.clone());
-    println!("The measures increased: {} times", count);
-    let count = count_increased(clean_measures(measures));
-    println!("The measures increased: {} times", count);
-    Ok(())
+pub struct Day1 {}
+
+impl Day<Vec<i32>, i32> for Day1 {
+    fn parse_input(input: &str) -> Result<Vec<i32>> {
+        let r = input
+            .lines()
+            .into_iter()
+            .filter_map(|s| s.parse().ok())
+            .collect();
+        Ok(r)
+    }
+
+    fn part_one(input: &Vec<i32>) -> Result<i32> {
+        Ok(count_increased(input.to_vec()))
+    }
+
+    fn part_two(input: &Vec<i32>) -> Result<i32> {
+        Ok(count_increased(clean_measures(input.to_vec())))
+    }
 }
 
 fn count_increased(measures: Vec<i32>) -> i32 {
@@ -35,20 +44,35 @@ fn clean_measures(measures: Vec<i32>) -> Vec<i32> {
     new_measurents
 }
 
-#[test]
-fn count_increased_test() {
-    let measures = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-    let count = count_increased(measures);
-    assert_eq!(7, count);
-}
+#[cfg(test)]
+mod tests_day1 {
+    use super::*;
+    use crate::runner::Runner;
+    use anyhow::Result;
 
-#[test]
-fn clean_measures_test() {
-    let measures = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-    let count = clean_measures(measures);
-    assert_eq!(8, count.len());
-    let mut it = count.into_iter();
-    assert_eq!(607, it.next().unwrap());
-    assert_eq!(618, it.next().unwrap());
-    assert_eq!(792, it.last().unwrap());
+    #[test]
+    fn run_test() -> Result<()> {
+        let (r1, r2) = Day1::run("inputs/day1.test")?;
+        assert_eq!(r1, 7);
+        assert_eq!(r2, 5);
+        Ok(())
+    }
+
+    #[test]
+    fn count_increased_test() {
+        let measures = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        let count = count_increased(measures);
+        assert_eq!(7, count);
+    }
+
+    #[test]
+    fn clean_measures_test() {
+        let measures = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        let count = clean_measures(measures);
+        assert_eq!(8, count.len());
+        let mut it = count.into_iter();
+        assert_eq!(607, it.next().unwrap());
+        assert_eq!(618, it.next().unwrap());
+        assert_eq!(792, it.last().unwrap());
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@ mod day_four;
 mod day_one;
 mod day_three;
 mod day_two;
+mod runner;
 
 use anyhow::Result;
+use day_one::Day1;
+use runner::Runner;
 
 fn main() -> Result<()> {
-    day_one::run_day_one()?;
+    Day1::run("inputs/day1.input")?;
     day_two::run_day_two()?;
     day_three::run_day_three()?;
     day_four::run_day_four("inputs/day4.input")?;
